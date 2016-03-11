b118fdc1e9b1aebdc178537551101dffe1f612a3
Tianon Gravi <admwiggin@gmail.com>
2016-03-11 15:11:27 -0800

    Restructure a bit so we can autogenerate alpine variants easily and consistently too (and add OpenJDK 7!)

--- update.sh
+++ update.sh
@@ -26,7 +26,26 @@ declare -A variants=(
 	[jdk]='scm'
 )
 
+alpineVersion='3.3'
+alpineMirror="http://dl-cdn.alpinelinux.org/alpine/v${alpineVersion}/community/x86_64"
+curl -fsSL'#' "$alpineMirror/APKINDEX.tar.gz" | tar -zxv APKINDEX
+
 declare -A debCache=()
+
+java-home-script() {
+	cat <<'EOD'
+
+# add a simple script that can auto-detect the appropriate JAVA_HOME value
+# based on whether the JDK or only the JRE is installed
+RUN { \
+		echo '#!/bin/sh'; \
+		echo 'set -e'; \
+		echo; \
+		echo 'dirname "$(dirname "$(readlink -f "$(which javac || which java)")")"'; \
+	} > /usr/local/bin/docker-java-home \
+	&& chmod +x /usr/local/bin/docker-java-home
+EOD
+}
 
 travisEnv=
 for version in "${versions[@]}"; do
@@ -92,18 +111,7 @@ for version in "${versions[@]}"; do
 		ENV LANG C.UTF-8
 	EOD
 
-	cat >> "$version/Dockerfile" <<EOD
-
-# add a simple script that can auto-detect the appropriate JAVA_HOME value
-# based on whether the JDK or only the JRE is installed
-RUN { \\
-		echo '#!/bin/bash'; \\
-		echo 'set -e'; \\
-		echo; \\
-		echo 'dirname "\$(dirname "\$(readlink -f "\$(which javac || which java)")")"'; \\
-	} > /usr/local/bin/docker-java-home \\
-	&& chmod +x /usr/local/bin/docker-java-home
-EOD
+	java-home-script >> "$version/Dockerfile"
 
 	cat >> "$version/Dockerfile" <<-EOD
 
@@ -155,7 +163,59 @@ EOD
 
 	variant='alpine'
 	if [ -d "$version/$variant" ]; then
-		# TODO ...
+		alpinePackage="openjdk$javaVersion"
+		alpineJavaHome="/usr/lib/jvm/java-1.${javaVersion}-openjdk"
+		case "$javaType" in
+			jdk)
+				;;
+			jre)
+				alpinePackage+="-$javaType"
+				alpineJavaHome+="/$javaType"
+				;;
+		esac
+		alpinePackageVersion="$(awk -F: '$1 == "P" { pkg = $2 } pkg == "'"$alpinePackage"'" && $1 == "V" { print $2 }' APKINDEX)"
+		alpineFullVersion="${alpinePackageVersion/./u}"
+		alpineFullVersion="${alpineFullVersion%%.*}"
+
+		cat > "$version/$variant/Dockerfile" <<-EOD
+			#
+			# NOTE: THIS DOCKERFILE IS GENERATED VIA "update.sh"
+			#
+			# PLEASE DO NOT EDIT IT DIRECTLY.
+			#
+
+			FROM alpine:$alpineVersion
+
+			# A few problems with compiling Java from source:
+			#  1. Oracle.  Licensing prevents us from redistributing the official JDK.
+			#  2. Compiling OpenJDK also requires the JDK to be installed, and it gets
+			#       really hairy.
+
+			# Default to UTF-8 file.encoding
+			ENV LANG C.UTF-8
+		EOD
+
+		java-home-script >> "$version/$variant/Dockerfile"
+
+		cat >> "$version/$variant/Dockerfile" <<-EOD
+			ENV JAVA_HOME $alpineJavaHome
+		EOD
+		cat >> "$version/$variant/Dockerfile" <<-'EOD'
+			ENV PATH $PATH:$JAVA_HOME/bin
+		EOD
+		cat >> "$version/$variant/Dockerfile" <<-EOD
+
+			ENV JAVA_VERSION $alpineFullVersion
+			ENV JAVA_ALPINE_VERSION $alpinePackageVersion
+		EOD
+		cat >> "$version/$variant/Dockerfile" <<EOD
+
+RUN set -x \\
+	&& apk add --no-cache \\
+		${alpinePackage}="\$JAVA_ALPINE_VERSION" \\
+	&& [ "\$JAVA_HOME" = "\$(docker-java-home)" ]
+EOD
+
 		travisEnv='\n  - VERSION='"$version"' VARIANT='"$variant$travisEnv"
 	fi
 
@@ -164,3 +224,5 @@ done
 
 travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
 echo "$travis" > .travis.yml
+
+rm APKINDEX

83fbf16d99f4094df192b4f07909b473ad1d8392
Tianon Gravi <admwiggin@gmail.com>
2020-07-27 11:05:31 -0700

    Refactor repo organization for better multi-version support

--- update.sh
+++ update.sh
@@ -109,15 +109,37 @@ jdk-java-net-download-version() {
 sed_escape_rhs() {
 	sed -e 's/[\/&]/\\&/g' <<<"$*" | sed -e ':a;N;$!ba;s/\n/\\n/g'
 }
+sed_s() {
+	local lhs="$1"; shift
+	local rhs="$1"; shift
+	rhs="$(sed_escape_rhs "$rhs")"
+	echo -n "s/$lhs/$rhs/g"
+}
+sed_s_pre() {
+	local lhs="$1"; shift
+	local rhs="$1"; shift
+	rhs="$(sed_escape_rhs "$rhs")"
+	echo -n "s/^($lhs) .*$/\1 $rhs/"
+}
 
 for javaVersion in "${versions[@]}"; do
 	for javaType in jdk jre; do
 		dir="$javaVersion/$javaType"
 		[ -d "$dir" ] || continue
 
+		downloadSource= # "adopt", "oracle"
+		linuxVersion= # "11.0.8", "15-ea+33", "8u262", etc
+		alpineVersion=
+		windowsVersion=
+		linuxArchCase=
+		alpineArchCase=
+		windowsDownloadUrl=
+		windowsDownloadSha256=
+
 		case "$javaVersion" in
 			8 | 11)
-				# TODO Dockerfile-adopt-*.template
+				downloadSource='adopt'
+
 				githubUrl="$(adopt-github-url "$javaVersion")"
 				sourcesUrl="$(adopt-sources-url "$githubUrl")"
 				adoptVersion="$(adopt-version "$githubUrl")"
@@ -126,71 +148,47 @@ for javaVersion in "${versions[@]}"; do
 				javaUrlVersion="${javaUrlVersion%.tar.gz}"
 				javaUrlBase+="${javaType}_" # "jre_", "jdk_", etc
 
-				echo "$javaVersion-$javaType: $adoptVersion ($javaUrlVersion; $javaUrlBase)"
-
-				sedArgs=(
-					-e 's!^(ENV JAVA_VERSION) .*!\1 '"$adoptVersion"'!'
-					-e 's!^(ENV JAVA_BASE_URL) .*!\1 '"$javaUrlBase"'!'
-					-e 's!^(ENV JAVA_URL_VERSION) .*!\1 '"$javaUrlVersion"'!'
+				possibleArches=(
+					# https://github.com/AdoptOpenJDK/openjdk8-upstream-binaries/releases
+					# https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases
+					'aarch64_linux'
+					'x64_linux'
+					'x64_windows'
 				)
-
-				case "$javaType" in
-					jdk)
-						baseFrom='buildpack-deps:buster-scm'
-						;;
-
-					jre)
-						baseFrom='buildpack-deps:buster-curl'
-						sedArgs+=( -e '/javac --version/d' )
-						;;
-
-					*) echo >&2 "echo: unexpected javaType: $javaType"; exit 1 ;;
-				esac
-
-				if [ "$javaVersion" = '8' ]; then
-					sedArgs+=(
-						# no "--" style flags on OpenJDK 8
-						-e 's! --version! -version!g'
-
-						# and no "jshell" until OpenJDK 9
-						-e '/jshell/d'
-					)
-				fi
-				if [ "$javaType" = 'jre' ]; then
-					# no "jshell" in JRE
-					sedArgs+=( -e '/jshell/d' )
-				fi
-
-				linuxSedArgs=(
-					-e 's!^(ENV JAVA_HOME) .*!\1 /usr/local/openjdk-'"$javaVersion"'!'
-					"${sedArgs[@]}"
-				)
-				sed -r "${linuxSedArgs[@]}" -e 's!^(FROM) .*!\1 '"$baseFrom"'!' Dockerfile-adopt-linux.template > "$dir/Dockerfile"
-				sed -r "${linuxSedArgs[@]}" Dockerfile-adopt-slim.template > "$dir/slim/Dockerfile"
-				dockerfiles=( "$dir/Dockerfile" "$dir/slim/Dockerfile" )
-
-				if [ -d "$dir/windows" ]; then
-					for winD in "$dir"/windows/*/; do
-						winD="${winD%/}"
-						windowsVersion="$(basename "$winD")"
-						windowsVariant="${windowsVersion%%-*}" # "windowsservercore", "nanoserver"
-						windowsVersion="${windowsVersion#$windowsVariant-}" # "1803", "ltsc2016", etc
-						windowsVariant="${windowsVariant#windows}" # "servercore", "nanoserver"
-						serverCoreImage="openjdk:$adoptVersion-$javaType-windowsservercore-$windowsVersion" # "openjdk:8u212-b04-jre-windowsservercore-1809", etc
-						sed -r "${sedArgs[@]}" \
-							-e 's!^(ENV JAVA_HOME) .*!\1 C:\\\\openjdk-'"$javaVersion"'!' \
-							-e 's!^(FROM) .*$!\1 mcr.microsoft.com/windows/'"$windowsVariant"':'"$windowsVersion"'!' \
-							-e 's!%%SERVERCORE-IMAGE%%!'"$serverCoreImage"'!g' \
-							"Dockerfile-adopt-windows-$windowsVariant.template" > "$winD/Dockerfile"
-						dockerfiles+=( "$winD/Dockerfile" )
-					done
-				fi
-
-				# remove any blank line at EOF that removing "jshell" in 8 leaves
-				sed -i -e '${/^$/d;}' "${dockerfiles[@]}"
+				for arch in "${possibleArches[@]}"; do
+					case "$arch" in
+						*_linux) downloadSuffix='.tar.gz' ;;
+						*_windows) downloadSuffix='.zip' ;;
+						*) echo >&2 "error: unknown Adopt Upstream arch: '$arch'"; exit 1 ;;
+					esac
+					downloadUrl="${javaUrlBase}${arch}_${javaUrlVersion}${downloadSuffix}"
+					downloadFile="$(basename "$downloadUrl")"
+					if curl -fsSL "$githubUrl" |tac|tac| grep -qF "$downloadFile"; then
+						case "$arch" in
+							*_windows)
+								windowsVersion="$adoptVersion"
+								windowsDownloadUrl="$downloadUrl"
+								;;
+							*_linux)
+								linuxVersion="$adoptVersion"
+								case "$arch" in
+									aarch64_*) caseArch='arm64 | aarch64'; bashbrewArch='arm64v8' ;;
+									x64_*) caseArch='amd64 | i386:x86-64'; bashbrewArch='amd64' ;;
+									*) echo >&2 "error: unknown Adopt Upstream linux arch: '$arch'"; exit 1 ;;
+								esac
+								newArchCase="$(printf '\t\t%s) downloadUrl=%q ;;' "$caseArch" "$downloadUrl")"
+								newArchCase="# $bashbrewArch"$'\n'"$newArchCase"$' \\\n'
+								linuxArchCase+="$newArchCase"
+								;;
+							*) echo >&2 "error: unknown Adopt Upstream arch: '$arch'"; exit 1 ;;
+						esac
+					fi
+				done
 				;;
 
 			14 | 15 | 16)
+				downloadSource='oracle'
+
 				possibleArches=(
 					# https://jdk.java.net/15/
 					# https://jdk.java.net/16/
@@ -199,14 +197,11 @@ for javaVersion in "${versions[@]}"; do
 					'linux-x64-musl'
 					'windows-x64'
 				)
-				declare -A archSha256s=()
-				declare -A archUrls=()
-				declare -A archVersions=()
-
 				for arch in "${possibleArches[@]}"; do
+					downloadSuffix="_${arch}_bin"
 					case "$arch" in
-						linux-*) downloadSuffix="_${arch}_bin.tar.gz" ;;
-						windows-*) downloadSuffix="_${arch}_bin.zip" ;;
+						linux-*) downloadSuffix+='.tar.gz' ;;
+						windows-*) downloadSuffix+='.zip' ;;
 						*) echo >&2 "error: unknown Oracle arch: '$arch'"; exit 1 ;;
 					esac
 					if downloadUrl="$(jdk-java-net-download-url "$javaVersion" "$downloadSuffix")" \
@@ -214,96 +209,46 @@ for javaVersion in "${versions[@]}"; do
 						&& downloadSha256="$(wget -qO- "$downloadUrl.sha256")" \
 						&& [ -n "$downloadSha256" ] \
 					; then
-						archSha256s["$arch"]="$downloadSha256"
-						archUrls["$arch"]="$downloadUrl"
 						downloadVersion="$(jdk-java-net-download-version "$javaVersion" "$downloadUrl")"
-						archVersions["$arch"]="$downloadVersion"
-					fi
-				done
-
-				arch='linux-x64-musl'
-				if [ -n "${archSha256s["$arch"]:-}" ]; then
-					downloadUrl="${archUrls["$arch"]}"
-					downloadSha256="${archSha256s["$arch"]}"
-					downloadVersion="${archVersions["$arch"]}"
-					unset archUrls["$arch"] archSha256s["$arch"] archVersions["$arch"]
-
-					echo "$javaVersion-$javaType: $downloadVersion (alpine)"
-
-					mkdir -p "$dir/alpine"
-
-					sed -r \
-						-e 's!^(ENV JAVA_HOME) .*!\1 /opt/openjdk-'"$javaVersion"'!' \
-						-e 's!^(ENV JAVA_VERSION) .*!\1 '"$downloadVersion"'!' \
-						-e 's!^(ENV JAVA_URL) .*!\1 '"$downloadUrl"'!' \
-						-e 's!^(ENV JAVA_SHA256) .*!\1 '"$downloadSha256"'!' \
-						Dockerfile-oracle-alpine.template > "$dir/alpine/Dockerfile"
-				else
-					rm -rf "$dir/alpine"
-				fi
-
-				arch='windows-x64'
-				if [ -n "${archSha256s["$arch"]:-}" ]; then
-					downloadUrl="${archUrls["$arch"]}"
-					downloadSha256="${archSha256s["$arch"]}"
-					downloadVersion="${archVersions["$arch"]}"
-					unset archUrls["$arch"] archSha256s["$arch"] archVersions["$arch"]
-
-					echo "$javaVersion-$javaType: $downloadVersion (windows)"
-
-					for winD in "$dir"/windows/*/; do
-						winD="${winD%/}"
-						windowsVersion="$(basename "$winD")"
-						windowsVariant="${windowsVersion%%-*}" # "windowsservercore", "nanoserver"
-						windowsVersion="${windowsVersion#$windowsVariant-}" # "1803", "ltsc2016", etc
-						windowsVariant="${windowsVariant#windows}" # "servercore", "nanoserver"
-						serverCoreImage="openjdk:${downloadVersion//+/-}-windowsservercore-$windowsVersion" # "openjdk:8u212-b04-windowsservercore-1809", etc
-						sed -r \
-							-e 's!^(FROM) .*$!\1 mcr.microsoft.com/windows/'"$windowsVariant"':'"$windowsVersion"'!' \
-							-e 's!^(ENV JAVA_HOME) .*!\1 C:\\\\openjdk-'"$javaVersion"'!' \
-							-e 's!^(ENV JAVA_VERSION) .*!\1 '"$downloadVersion"'!' \
-							-e 's!^(ENV JAVA_URL) .*!\1 '"$downloadUrl"'!' \
-							-e 's!^(ENV JAVA_SHA256) .*!\1 '"$downloadSha256"'!' \
-							-e 's!%%SERVERCORE-IMAGE%%!'"$serverCoreImage"'!g' \
-							"Dockerfile-oracle-windows-$windowsVariant.template" > "$winD/Dockerfile"
-					done
-				else
-					rm -rf "$dir/windows"
-				fi
-
-				downloadVersion="${archVersions['linux-x64']}"
-				linuxArchCase=$'case "$arch" in \\\n'
-				for arch in "${possibleArches[@]}"; do
-					if [ -n "${archSha256s["$arch"]:-}" ]; then
-						if [ "$downloadVersion" != "${archVersions["$arch"]}" ]; then
-							echo >&2 "error: version for '$arch' does not match 'linux-x64': $downloadVersion vs ${archVersions["$arch"]}"
-							exit 1
-						fi
 						case "$arch" in
-							# dpkg-architecture | "objdump --file-headers /sbin/init | awk -F '[:,]+[[:space:]]+' '$1 == "architecture" { print $2 }'"
-							*-x64) caseArch='amd64 | i386:x86-64'; bashbrewArch='amd64' ;;
-							*-aarch64) caseArch='arm64 | aarch64'; bashbrewArch='arm64v8' ;;
-							*) echo >&2 "error: unknown Oracle case arch: '$arch'"; exit 1 ;;
+							windows-*)
+								windowsVersion="$downloadVersion"
+								windowsDownloadUrl="$downloadUrl"
+								windowsDownloadSha256="$downloadSha256"
+								;;
+							linux-*)
+								if [[ "$arch" == *-musl ]]; then
+									if [ -z "$alpineVersion" ] || [ "$alpineVersion" = "$downloadVersion" ]; then
+										alpineVersion="$downloadVersion"
+									else
+										echo >&2 "error: mismatched Alpine versions! ('$alpineVersion' vs '$downloadVersion')"
+										exit 1
+									fi
+								else
+									if [ -z "$linuxVersion" ] || [ "$linuxVersion" = "$downloadVersion" ]; then
+										linuxVersion="$downloadVersion"
+									else
+										echo >&2 "error: mismatched Linux versions! ('$linuxVersion' vs '$downloadVersion')"
+										exit 1
+									fi
+								fi
+								case "$arch" in
+									linux-aarch64) caseArch='arm64 | aarch64'; bashbrewArch='arm64v8' ;;
+									linux-aarch64-musl) caseArch='aarch64'; bashbrewArch='arm64v8' ;;
+									linux-x64) caseArch='amd64 | i386:x86-64'; bashbrewArch='amd64' ;;
+									linux-x64-musl) caseArch='x86_64'; bashbrewArch='amd64' ;;
+									*) echo >&2 "error: unknown Alpine Oracle arch: '$arch'"; exit 1 ;;
+								esac
+								newArchCase="$(printf '\t\t%s) \\\n\t\t\tdownloadUrl=%q; \\\n\t\t\tdownloadSha256=%q; \\\n\t\t\t;;' "$caseArch" "$downloadUrl" "$downloadSha256")"
+								newArchCase="# $bashbrewArch"$'\n'"$newArchCase"$' \\\n'
+								if [[ "$arch" == *-musl ]]; then
+									alpineArchCase+="$newArchCase"
+								else
+									linuxArchCase+="$newArchCase"
+								fi
+								;;
 						esac
-						linuxArchCase+="# $bashbrewArch"$'\n'
-						newArchCase="$(printf '\t\t%s) \\\n\t\t\tdownloadUrl=%q; \\\n\t\t\tdownloadSha256=%q; \\\n\t\t\t;;' "$caseArch" "${archUrls["$arch"]}" "${archSha256s["$arch"]}")"
-						linuxArchCase+="$newArchCase"$' \\\n'
 					fi
-				done
-				linuxArchCase+=$'# fallback\n'
-				linuxArchCase+=$'\t\t*) echo >&2 "error: unsupported architecture: \'$arch\'"; exit 1 ;; \\\n'
-				linuxArchCase+=$'\tesac'
-
-				echo "$javaVersion-$javaType: $downloadVersion (oracle); ${!archSha256s[*]}"
-
-				for variant in oracle debian slim; do
-					[ "$variant" = 'debian' ] && variantDir="$dir" || variantDir="$dir/$variant"
-					mkdir -p "$variantDir"
-					sed -r \
-						-e 's!^(ENV JAVA_HOME) .*!\1 /usr/java/openjdk-'"$javaVersion"'!' \
-						-e 's!^(ENV JAVA_VERSION) .*!\1 '"$downloadVersion"'!' \
-						-e 's!%%ARCH-CASE%%!'"$(sed_escape_rhs "$linuxArchCase")"'!g' \
-						"Dockerfile-oracle-$variant.template" > "$variantDir/Dockerfile"
 				done
 				;;
 
@@ -312,5 +257,127 @@ for javaVersion in "${versions[@]}"; do
 				exit 1
 				;;
 		esac
+
+		if [ -z "$downloadSource" ]; then
+			echo >&2 "error: missing download source for $javaVersion-$javaType"
+			exit 1
+		fi
+
+		echo "$javaVersion-$javaType: $linuxVersion ($downloadSource)"
+		if [ -n "$alpineVersion" ] && [ "$linuxVersion" != "$alpineVersion" ]; then
+			echo "  - alpine: $alpineVersion"
+		fi
+		if [ -n "$windowsVersion" ] && [ "$linuxVersion" != "$windowsVersion" ]; then
+			echo "  - windows: $windowsVersion"
+		fi
+
+		# add "arch case" boilerplate
+		archCasePrefix=$'case "$arch" in \\\n'
+		archCaseSuffix=$'# fallback\n'
+		archCaseSuffix+=$'\t\t*) echo >&2 "error: unsupported architecture: \'$arch\'"; exit 1 ;; \\\n'
+		archCaseSuffix+=$'\tesac'
+		linuxArchCase="${archCasePrefix}${linuxArchCase}${archCaseSuffix}"
+		alpineArchCase="${archCasePrefix}${alpineArchCase}${archCaseSuffix}"
+
+		for variant in \
+			oraclelinux7 \
+			{,slim-}buster \
+			alpine3.12 \
+			windows/windowsservercore-{1809,ltsc2016} \
+			windows/nanoserver-1809 \
+		; do
+			[ -d "$dir/$variant" ] || continue
+
+			sedArgs=( -r )
+			variantVersion=
+			variantJavaHome=
+			variantArchCase=
+
+			case "$variant" in
+				alpine*)
+					template="Dockerfile-$downloadSource-alpine.template"
+					from="alpine:${variant#alpine}"
+					variantVersion="$alpineVersion"
+					variantJavaHome="/opt/openjdk-$javaVersion"
+					variantArchCase="$alpineArchCase"
+					;;
+				oraclelinux*)
+					template="Dockerfile-$downloadSource-oraclelinux.template"
+					from="oraclelinux:${variant#oraclelinux}-slim"
+					variantVersion="$linuxVersion"
+					variantJavaHome="/usr/java/openjdk-$javaVersion"
+					variantArchCase="$linuxArchCase"
+					;;
+				windows/*)
+					variantVersion="$windowsVersion"
+					variantJavaHome="C:\\\\openjdk-$javaVersion"
+					windowsRelease="$(basename "$variant")" # "windowsservercore-1809", "nanoserver-1809", etc
+					windowsVariant="${windowsRelease%%-*}" # "windowsservercore", "nanoserver"
+					windowsRelease="${windowsRelease#$windowsVariant-}" # "1809", "ltsc2016", etc
+					windowsVariant="${windowsVariant#windows}" # "servercore", "nanoserver"
+					template="Dockerfile-$downloadSource-windows-$windowsVariant.template"
+					from="mcr.microsoft.com/windows/$windowsVariant:$windowsRelease"
+					if [ "$windowsVariant" = 'nanoserver' ]; then
+						servercore="openjdk:${variantVersion//+/-}-$javaType-windowsservercore-$windowsRelease"
+						sedArgs+=( -e "$(sed_s '%%SERVERCORE-IMAGE%%' "$servercore")" )
+					fi
+					sedArgs+=( -e "$(sed_s_pre 'ENV JAVA_URL' "$windowsDownloadUrl")" )
+					[ -z "$windowsDownloadSha256" ] || sedArgs+=( -e "$(sed_s_pre 'ENV JAVA_SHA256' "$windowsDownloadSha256")" )
+					;;
+				slim-*)
+					template="Dockerfile-$downloadSource-debian-slim.template"
+					from="debian:${variant#slim-}-slim"
+					variantVersion="$linuxVersion"
+					variantJavaHome="/usr/local/openjdk-$javaVersion"
+					variantArchCase="$linuxArchCase"
+					;;
+				*)
+					template="Dockerfile-$downloadSource-debian.template"
+					case "$javaType" in
+						jdk) from="buildpack-deps:$variant-scm" ;;
+						jre) from="buildpack-deps:$variant-curl" ;;
+					esac
+					variantVersion="$linuxVersion"
+					variantJavaHome="/usr/local/openjdk-$javaVersion"
+					variantArchCase="$linuxArchCase"
+					;;
+			esac
+
+			sedArgs+=(
+				-e "$(sed_s_pre 'FROM' "$from")"
+				-e "$(sed_s_pre 'ENV JAVA_VERSION' "$variantVersion")"
+				-e "$(sed_s_pre 'ENV JAVA_HOME' "$variantJavaHome")"
+			)
+			[ -z "$variantArchCase" ] || sedArgs+=( -e "$(sed_s '%%ARCH-CASE%%' "$variantArchCase")" )
+
+			case "$javaType" in
+				jre)
+					sedArgs+=(
+						# no javac or jshell in JRE
+						-e '/javac --version/d'
+						-e '/jshell/d'
+					)
+					;;
+			esac
+
+			if [ "$javaVersion" = '8' ]; then
+				sedArgs+=(
+					# no "--" style flags on OpenJDK 8
+					-e 's! --version! -version!g'
+
+					# and no "jshell" until OpenJDK 9
+					-e '/jshell/d'
+				)
+			fi
+
+			if [ -z "$variantVersion" ]; then
+				echo >&2 "warning: missing '$dir/$variant' version!"
+				rm -f "$dir/$variant/Dockerfile"
+				continue
+			fi
+
+			# extra sed to remove any blank line at EOF that removing "jshell" leaves behind
+			sed "${sedArgs[@]}" "$template" | sed -e '${/^$/d;}' > "$dir/$variant/Dockerfile"
+		done
 	done
 done
